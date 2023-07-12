907e8259652132e2932b8ab0dac99bdb60c1706d
scooter7 <scooter.vineburgh@gmail.com>
2023-07-11 22:55:50 -0500

    Update cxpq5.py

--- cxpq5.py
+++ cxpq5.py
@@ -58,8 +58,115 @@ def personality_quiz():
         return randomized_options
 
     def run_quiz():
-        # Quiz logic goes here
-        # ...
+        for answer in selected_traits_q1:
+            score_counter[trait_score_map[answer]] += 1
+        score_counter[trait_score_map[selected_single_trait_q2]] += 1
+        for answer in least_represented_traits_q3:
+            score_counter[trait_score_map[answer]] -= 1
+        for answer in selected_traits_q4:
+            score_counter[trait_score_map[answer]] += 1
+        score_counter[trait_score_map[selected_single_trait_q5]] += 1
+        for answer in least_represented_traits_q6:
+            score_counter[trait_score_map[answer]] -= 1
+        for image in selected_images_q7:
+            score_counter[image_score_map[image]] += 1
+        score_counter[image_score_map[selected_image_q8]] += 1
+        for image in least_represented_images_q9:
+            score_counter[image_score_map[image]] -= 1
+        for mode in selected_modes_q10:
+            score_counter[trait_score_map[mode]] += 1
+
+        sorted_scores = sorted(score_counter.items(), key=lambda item: (-item[1], color_priority.index(item[0])))
+        top_two_colors = [color for color, _ in sorted_scores[:2]]
+        persona_name = get_persona_name(top_two_colors[0], top_two_colors[1])
+        return top_two_colors, persona_name, score_counter
+
+    def get_persona_name(primary_color, secondary_color):
+        persona_map = {
+            ("Blue", "Maroon"): "Champion",
+            ("Blue", "Green"): "Captain",
+            ("Blue", "Orange"): "Director",
+            ("Blue", "Pink"): "Producer",
+            ("Blue", "Purple"): "Mentor",
+            ("Blue", "Red"): "Coach",
+            ("Blue", "Silver"): "Maverick",
+            ("Blue", "Yellow"): "Visionary",
+            ("Blue", "Beige"): "Achiever",
+            ("Maroon", "Blue"): "Contender",
+            ("Maroon", "Green"): "Need to Find",
+            ("Maroon", "Orange"): "Maker",
+            ("Maroon", "Pink"): "Precisionist",
+            ("Maroon", "Purple"): "Protector",
+            ("Maroon", "Red"): "Energizer",
+            ("Maroon", "Silver"): "Dark Horse",
+            ("Maroon", "Yellow"): "Challenger",
+            ("Maroon", "Beige"): "Competitor",
+            ("Green", "Blue"): "Trailblazer",
+            ("Green", "Maroon"): "Adventurer",
+            ("Green", "Orange"): "Seeker",
+            ("Green", "Pink"): "Detective",
+            ("Green", "Purple"): "Ambassador",
+            ("Green", "Red"): "Globetrotter",
+            ("Green", "Silver"): "Ranger",
+            ("Green", "Yellow"): "Researcher",
+            ("Green", "Beige"): "Explorer",
+            ("Orange", "Blue"): "Architect",
+            ("Orange", "Maroon"): "Artisan",
+            ("Orange", "Green"): "Searcher",
+            ("Orange", "Pink"): "Composer",
+            ("Orange", "Purple"): "Curator",
+            ("Orange", "Red"): "Storyteller",
+            ("Orange", "Silver"): "Nonconformist",
+            ("Orange", "Yellow"): "Ideator",
+            ("Orange", "Beige"): "Creator",
+            ("Pink", "Blue"): "Connoisseur",
+            ("Pink", "Maroon"): "Perfectionist",
+            ("Pink", "Green"): "Philosopher",
+            ("Pink", "Orange"): "Virtuoso",
+            ("Pink", "Purple"): "Idealist",
+            ("Pink", "Red"): "Aficionado",
+            ("Pink", "Silver"): "Refiner",
+            ("Pink", "Yellow"): "Trendsetter",
+            ("Pink", "Beige"): "Sophisticate",
+            ("Purple", "Blue"): "Guide",
+            ("Purple", "Maroon"): "Guardian",
+            ("Purple", "Green"): "Shepherd",
+            ("Purple", "Orange"): "Patron",
+            ("Purple", "Pink"): "Confidant",
+            ("Purple", "Red"): "Host",
+            ("Purple", "Silver"): "Advocate",
+            ("Purple", "Yellow"): "Advisor",
+            ("Purple", "Beige"): "Provider",
+            ("Red", "Blue"): "Motivator",
+            ("Red", "Maroon"): "Dynamo",
+            ("Red", "Green"): "Thrill-seeker",
+            ("Red", "Orange"): "Performer",
+            ("Red", "Pink"): "Enthusiast",
+            ("Red", "Purple"): "Emcee",
+            ("Red", "Silver"): "DaRedevil",
+            ("Red", "Yellow"): "Magician",
+            ("Red", "Beige"): "Entertainer",
+            ("Silver", "Blue"): "Ringleader",
+            ("Silver", "Maroon"): "Instigator",
+            ("Silver", "Green"): "Rogue",
+            ("Silver", "Orange"): "Renegade",
+            ("Silver", "Pink"): "Individualist",
+            ("Silver", "Purple"): "Activist",
+            ("Silver", "Red"): "Rock Star",
+            ("Silver", "Yellow"): "Free-thinker",
+            ("Silver", "Beige"): "Rebel",
+            ("Yellow", "Blue"): "Vanguard",
+            ("Yellow", "Maroon"): "Inventor",
+            ("Yellow", "Green"): "Theorist",
+            ("Yellow", "Orange"): "Originator",
+            ("Yellow", "Pink"): "Dreamer",
+            ("Yellow", "Purple"): "Oracle",
+            ("Yellow", "Red"): "Futurist",
+            ("Yellow", "Silver"): "Reformer",
+            ("Yellow", "Beige"): "Innovator"
+        }
+
+        return persona_map.get((primary_color, secondary_color), "")
 
     st.title('CollegeXpress Personality Survey')
 
@@ -77,18 +184,18 @@ def personality_quiz():
 
     traits = randomize_options(traits)
 
-    st.write("Q1. Here is a list of 9 traits that could make up your personality. "
-             "Please select exactly 3 traits that best represent who you are.")
     selected_traits_q1 = []
     for trait in traits:
         selected = st.checkbox(trait, key=f"checkbox_q1_{trait}")
         if selected:
             selected_traits_q1.append(trait)
 
+    if len(selected_traits_q1) != 3:
+        st.warning("Please select exactly 3 traits.")
+
     st.write("---")
 
     if len(selected_traits_q1) == 3:
-        st.write("Q2. Of the 3 traits you selected, which single trait is most like you?")
         selected_single_trait_q2 = st.radio("", selected_traits_q1, key="radio_q2")
 
         st.write("---")
@@ -103,6 +210,9 @@ def personality_quiz():
             selected = st.checkbox(trait, key=f"checkbox_q3_{trait}")
             if selected:
                 least_represented_traits_q3.append(trait)
+
+        if len(least_represented_traits_q3) != 3:
+            st.warning("Please select exactly 3 traits.")
 
         st.write("---")
 
@@ -129,10 +239,12 @@ def personality_quiz():
                 if selected:
                     selected_traits_q4.append(trait)
 
+            if len(selected_traits_q4) != 3:
+                st.warning("Please select exactly 3 traits.")
+
             st.write("---")
 
             if len(selected_traits_q4) == 3:
-                st.write("Q5. Of the 3 traits you selected, which single trait is most like you?")
                 selected_single_trait_q5 = st.radio("", selected_traits_q4, key="radio_q5")
 
                 st.write("---")
@@ -225,6 +337,9 @@ def personality_quiz():
                                     least_represented_images_q9.append(file)
                                 st.image(image, use_column_width=True)
 
+                            if len(least_represented_images_q9) != 3:
+                                st.warning("Please select exactly 3 images.")
+
                             st.write("---")
 
                             if len(least_represented_images_q9) == 3:
@@ -250,6 +365,9 @@ def personality_quiz():
                                     selected = st.checkbox(mode, key=f"checkbox_q10_{mode}")
                                     if selected:
                                         selected_modes_q10.append(mode)
+
+                                if len(selected_modes_q10) != 2:
+                                    st.warning("Please select exactly 2 modes.")
 
                                 st.write("---")
 
