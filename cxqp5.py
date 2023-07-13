2722b862b58549c3534a417dac276f97492e122e
scooter7 <scooter.vineburgh@gmail.com>
2023-07-13 09:48:03 -0500

    Update cxqp5.py

--- cxqp5.py
+++ cxqp5.py
@@ -6,19 +6,14 @@ from io import BytesIO
 import random
 import hashlib
 
-# Function to create a seed from the user session and question number
 def create_seed(session_id, question_number):
-    # Use a hash function to ensure the seed is an integer
     seed = int(hashlib.sha256(f"{session_id}{question_number}".encode()).hexdigest(), 16) % 10**8
     return seed
 
-# Function to randomize a list using a seed
-def randomize_list(list_str, seed):
-    options = list_str.split(', ')
+def randomize_list(options, seed):
     random.seed(seed)
     random_options = random.sample(options, len(options))
-    random_list_str = ', '.join(random_options)
-    return random_list_str
+    return random_options
 
 def personality_quiz():
     trait_score_map = {
@@ -65,7 +60,7 @@ def personality_quiz():
 
     color_priority = ["Pink", "Blue", "Silver", "Yellow", "Maroon", "Red", "Orange", "Green", "Purple"]
 
-    score_counter = Counter({color: 3 for color in color_priority})  # Start with 3 points for each color
+    score_counter = Counter({color: 3 for color in color_priority}) 
 
     def run_quiz():
         for answer in selected_traits_q1:
@@ -95,17 +90,93 @@ def personality_quiz():
         persona_map = {
             ("Blue", "Maroon"): "Champion",
             ("Blue", "Green"): "Captain",
-            # ... (omitted for brevity)
+            ("Blue", "Orange"): "Director",
+            ("Blue", "Pink"): "Producer",
+            ("Blue", "Purple"): "Mentor",
+            ("Blue", "Red"): "Coach",
+            ("Blue", "Silver"): "Maverick",
+            ("Blue", "Yellow"): "Visionary",
+            ("Blue", "Beige"): "Achiever",
+            ("Maroon", "Blue"): "Contender",
+            ("Maroon", "Green"): "Need to Find",
+            ("Maroon", "Orange"): "Maker",
+            ("Maroon", "Pink"): "Precisionist",
+            ("Maroon", "Purple"): "Protector",
+            ("Maroon", "Red"): "Energizer",
+            ("Maroon", "Silver"): "Dark Horse",
+            ("Maroon", "Yellow"): "Challenger",
+            ("Maroon", "Beige"): "Competitor",
+            ("Green", "Blue"): "Trailblazer",
+            ("Green", "Maroon"): "Adventurer",
+            ("Green", "Orange"): "Seeker",
+            ("Green", "Pink"): "Detective",
+            ("Green", "Purple"): "Ambassador",
+            ("Green", "Red"): "Globetrotter",
+            ("Green", "Silver"): "Ranger",
+            ("Green", "Yellow"): "Researcher",
+            ("Green", "Beige"): "Explorer",
+            ("Orange", "Blue"): "Architect",
+            ("Orange", "Maroon"): "Artisan",
+            ("Orange", "Green"): "Searcher",
+            ("Orange", "Pink"): "Composer",
+            ("Orange", "Purple"): "Curator",
+            ("Orange", "Red"): "Storyteller",
+            ("Orange", "Silver"): "Nonconformist",
+            ("Orange", "Yellow"): "Ideator",
+            ("Orange", "Beige"): "Creator",
+            ("Pink", "Blue"): "Connoisseur",
+            ("Pink", "Maroon"): "Perfectionist",
+            ("Pink", "Green"): "Philosopher",
+            ("Pink", "Orange"): "Virtuoso",
+            ("Pink", "Purple"): "Idealist",
+            ("Pink", "Red"): "Aficionado",
+            ("Pink", "Silver"): "Refiner",
+            ("Pink", "Yellow"): "Trendsetter",
+            ("Pink", "Beige"): "Sophisticate",
+            ("Purple", "Blue"): "Guide",
+            ("Purple", "Maroon"): "Guardian",
+            ("Purple", "Green"): "Shepherd",
+            ("Purple", "Orange"): "Patron",
+            ("Purple", "Pink"): "Confidant",
+            ("Purple", "Red"): "Host",
+            ("Purple", "Silver"): "Advocate",
+            ("Purple", "Yellow"): "Advisor",
+            ("Purple", "Beige"): "Provider",
+            ("Red", "Blue"): "Motivator",
+            ("Red", "Maroon"): "Dynamo",
+            ("Red", "Green"): "Thrill-seeker",
+            ("Red", "Orange"): "Performer",
+            ("Red", "Pink"): "Enthusiast",
+            ("Red", "Purple"): "Emcee",
+            ("Red", "Silver"): "DaReddevil",
+            ("Red", "Yellow"): "Magician",
+            ("Red", "Beige"): "Entertainer",
+            ("Silver", "Blue"): "Ringleader",
+            ("Silver", "Maroon"): "Instigator",
+            ("Silver", "Green"): "Rogue",
+            ("Silver", "Orange"): "Renegade",
+            ("Silver", "Pink"): "Individualist",
+            ("Silver", "Purple"): "Activist",
+            ("Silver", "Red"): "Rock Star",
+            ("Silver", "Yellow"): "Free-thinker",
+            ("Silver", "Beige"): "Rebel",
+            ("Yellow", "Blue"): "Vanguard",
+            ("Yellow", "Maroon"): "Inventor",
+            ("Yellow", "Green"): "Theorist",
+            ("Yellow", "Orange"): "Originator",
+            ("Yellow", "Pink"): "Dreamer",
+            ("Yellow", "Purple"): "Oracle",
+            ("Yellow", "Red"): "Futurist",
+            ("Yellow", "Silver"): "Reformer",
+            ("Yellow", "Beige"): "Innovator"
         }
+
         return persona_map.get((primary_color, secondary_color), "")
 
     st.title('CollegeXpress Personality Survey')
 
-    # Placeholder for the user session ID
-    # In a real application, this should be replaced with the actual session ID
     session_id = 1
 
-    # Randomize the traits for question 1
     traits_q1 = [
         "Confident",
         "Curious",
@@ -119,110 +190,131 @@ def personality_quiz():
     ]
     traits_q1 = randomize_list(traits_q1, create_seed(session_id, 1))
 
-    st.write("Q1. Here is a list of 9 traits that could make up your personality. "
-             "Please select exactly 3 traits that best represent who you are.")
-    selected_traits_q1 = st.multiselect("", traits_q1, key="multiselect_q1")
-    if len(selected_traits_q1) != 3:
-        st.warning("Please select exactly 3 traits.")
-    else:
-        # Q2
-        st.write("Q2. Of the 3 traits you selected, which single trait is most like you?")
-        selected_single_trait_q2 = st.radio("", selected_traits_q1, key="radio_q2")
+    selected_traits_q1 = []
+    for trait in traits_q1:
+        selected = st.checkbox(trait, key=f"checkbox_q1_{trait}")
+        if selected:
+            selected_traits_q1.append(trait)
 
-        # Q3
-        st.write("Q3. Now think about this list and select the 3 traits that least represent who you are.")
-        remaining_traits_q3 = [trait for trait in traits_q1 if trait not in selected_traits_q1]
-        least_represented_traits_q3 = st.multiselect("", remaining_traits_q3, key="multiselect_q3")
-        if len(least_represented_traits_q3) != 3:
-            st.warning("Please select exactly 3 traits.")
-        else:
-            # Q4
-            traits_q4 = [
-                "Influential",
-                "Adventurous",
-                "Tough",
-                "Expressive",
-                "Polished",
-                "Selfless",
-                "Playful",
-                "Independent",
-                "Analytical"
-            ]
-            traits_q4 = randomize_list(traits_q4, create_seed(session_id, 4))
+    selected_single_trait_q2 = st.radio("", selected_traits_q1, key="radio_q2")
 
-            st.write("Q4. Here is a new list of 9 traits that could make up your personality. "
-                     "Please select exactly 3 traits that best represent who you are.")
-            selected_traits_q4 = st.multiselect("", traits_q4, key="multiselect_q4")
-            if len(selected_traits_q4) != 3:
-                st.warning("Please select exactly 3 traits.")
+    remaining_traits_q3 = [trait for trait in traits_q1 if trait not in selected_traits_q1]
+
+    least_represented_traits_q3 = []
+    for trait in remaining_traits_q3:
+        selected = st.checkbox(trait, key=f"checkbox_q3_{trait}")
+        if selected:
+            least_represented_traits_q3.append(trait)
+
+    traits_q4 = [
+        "Influential",
+        "Adventurous",
+        "Tough",
+        "Expressive",
+        "Polished",
+        "Selfless",
+        "Playful",
+        "Independent",
+        "Analytical"
+    ]
+    traits_q4 = randomize_list(traits_q4, create_seed(session_id, 4))
+
+    selected_traits_q4 = []
+    for trait in traits_q4:
+        selected = st.checkbox(trait, key=f"checkbox_q4_{trait}")
+        if selected:
+            selected_traits_q4.append(trait)
+
+    selected_single_trait_q5 = st.radio("", selected_traits_q4, key="radio_q5")
+
+    remaining_traits_q6 = [trait for trait in traits_q4 if trait not in selected_traits_q4]
+
+    least_represented_traits_q6 = []
+    for trait in remaining_traits_q6:
+        selected = st.checkbox(trait, key=f"checkbox_q6_{trait}")
+        if selected:
+            least_represented_traits_q6.append(trait)
+
+    image_files_q7 = [
+        "OrangeSet.jpg",
+        "BrownSet.jpg",
+        "RedSet.jpg",
+        "YellowSet.jpg",
+        "PurpleSet.jpg",
+        "BlueSet.jpg",
+        "GreenSet.jpg",
+        "PinkSet.jpg",
+        "BlackSet.jpg"
+    ]
+
+    selected_images_q7 = []
+
+    for i, file in enumerate(image_files_q7):
+        image_url = f"https://raw.githubusercontent.com/scooter7/cxpq/main/{file}"
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
+        selected = st.checkbox("", key=f"q7_{i}")
+        if selected:
+            selected_images_q7.append(file)
+        st.image(image, use_column_width=True)
+
+    selected_image_q8 = None
+
+    for i, file in enumerate(selected_images_q7):
+        image_url = f"https://raw.githubusercontent.com/scooter7/cxpq/main/{file}"
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
+        selected = st.checkbox("", key=f"q8_{i}")
+        st.image(image, use_column_width=True)
+        if selected:
+            if selected_image_q8:
+                st.warning("Please select only one image.")
             else:
-                # Q5
-                st.write("Q5. Of the 3 traits you selected, which single trait is most like you?")
-                selected_single_trait_q5 = st.radio("", selected_traits_q4, key="radio_q5")
+                selected_image_q8 = file
 
-                # Q6
-                st.write("Q6. Now think about this list and select the 3 traits that least represent who you are.")
-                remaining_traits_q6 = [trait for trait in traits_q4 if trait not in selected_traits_q4]
-                least_represented_traits_q6 = st.multiselect("", remaining_traits_q6, key="multiselect_q6")
-                if len(least_represented_traits_q6) != 3:
-                    st.warning("Please select exactly 3 traits.")
-                else:
-                    # Q7
-                    st.write("Q7. On this page there are 9 groups of icons meant to represent personalities. "
-                             "Please take a moment to view all the groups. Then select the 3 that best represent who you are.")
-                    image_files_q7 = [
-                        "OrangeSet.jpg",
-                        "BrownSet.jpg",
-                        "RedSet.jpg",
-                        "YellowSet.jpg",
-                        "PurpleSet.jpg",
-                        "BlueSet.jpg",
-                        "GreenSet.jpg",
-                        "PinkSet.jpg",
-                        "BlackSet.jpg"
-                    ]
-                    selected_images_q7 = st.multiselect("", image_files_q7, key="multiselect_q7")
-                    if len(selected_images_q7) != 3:
-                        st.warning("Please select exactly 3 images.")
-                    else:
-                        # Q8
-                        st.write("Q8. Of the 3 you selected, which group of icons is most like you?")
-                        selected_image_q8 = st.radio("", selected_images_q7, key="radio_q8")
+    if selected_image_q8:
+        image_url = f"https://raw.githubusercontent.com/scooter7/cxpq/main/{selected_image_q8}"
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
+        st.image(image, use_column_width=True)
 
-                        # Q9
-                        st.write("Q9. Now think about these icon groups remaining and select the 3 that least represent who you are.")
-                        remaining_images_q9 = [image for image in image_files_q7 if image not in selected_images_q7]
-                        least_represented_images_q9 = st.multiselect("", remaining_images_q9, key="multiselect_q9")
-                        if len(least_represented_images_q9) != 3:
-                            st.warning("Please select exactly 3 images.")
-                        else:
-                            # Q10
-                            st.write("Q10. Below are 9 things called 'Modes of Connection.' They describe how a person can make an impression, grow friendships, and inspire others. "
-                                     "Which two 'Modes of Connection' sound most like what you would use to make an impression, grow friendships, and inspire others?")
-                            modes_of_connection = [
-                                "Achieve With Me",
-                                "Explore With Me",
-                                "Strive With Me",
-                                "Create With Me",
-                                "Refine With Me",
-                                "Care With Me",
-                                "Enjoy With Me",
-                                "Defy With Me",
-                                "Invent With Me"
-                            ]
-                            modes_of_connection = randomize_list(modes_of_connection, create_seed(session_id, 10))
-                            selected_modes_q10 = st.multiselect("", modes_of_connection, key="multiselect_q10")
-                            if len(selected_modes_q10) != 2:
-                                st.warning("Please select exactly 2 modes.")
-                            else:
-                                # Submit
-                                st.write("Please click 'Submit' once you have completed the quiz.")
-                                if st.button("Submit"):
-                                    top_two_colors, persona_name, score_counter = run_quiz()
-                                    st.write("Your top two colors are: ", ", ".join(top_two_colors))
-                                    st.write("Your persona name is: ", persona_name)
-                                    st.write("Total Scores for Each Color:")
-                                    for color in color_priority:
-                                        st.write(f"{color}: {score_counter[color]}")
+    remaining_images_q9 = [file for file in image_files_q7 if file not in selected_images_q7]
+
+    least_represented_images_q9 = []
+
+    for i, file in enumerate(remaining_images_q9):
+        image_url = f"https://raw.githubusercontent.com/scooter7/cxpq/main/{file}"
+        response = requests.get(image_url)
+        image = Image.open(BytesIO(response.content))
+        selected = st.checkbox("", key=f"q9_{i}")
+        if selected:
+            least_represented_images_q9.append(file)
+        st.image(image, use_column_width=True)
+
+    modes_of_connection = [
+        "Achieve With Me",
+        "Explore With Me",
+        "Strive With Me",
+        "Create With Me",
+        "Refine With Me",
+        "Care With Me",
+        "Enjoy With Me",
+        "Defy With Me",
+        "Invent With Me"
+    ]
+    modes_of_connection = randomize_list(modes_of_connection, create_seed(session_id, 10))
+
+    selected_modes_q10 = []
+    for mode in modes_of_connection:
+        selected = st.checkbox(mode, key=f"checkbox_q10_{mode}")
+        if selected:
+            selected_modes_q10.append(mode)
+
+    if st.button("Submit"):
+        top_two_colors, persona_name, score_counter = run_quiz()
+        st.write("Your top two colors are: ", ", ".join(top_two_colors))
+        st.write("Your persona name is: ", persona_name)
+        for color in color_priority:
+            st.write(f"{color}: {score_counter[color]}")
 
 personality_quiz()
