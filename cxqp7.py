2da575f6952044571b0a70d61cc1f00d78fa5d94
scooter7 <scooter.vineburgh@gmail.com>
2023-07-15 12:44:00 -0500

    Update cxqp7.py

--- cxqp7.py
+++ cxqp7.py
@@ -206,33 +206,34 @@ def personality_quiz():
 
                                 st.write("Please click 'Submit' once you have completed the quiz.")
                                 if st.button("Submit"):
-                                    if len(selected_traits_q1) != 3:
-                                        st.warning("Please select exactly 3 traits for Q1.")
-                                    elif not selected_single_trait_q2:
-                                        st.warning("Please select a single trait for Q2.")
-                                    elif len(least_represented_traits_q3) != 3:
-                                        st.warning("Please select exactly 3 traits for Q3.")
-                                    elif len(selected_traits_q4) != 3:
-                                        st.warning("Please select exactly 3 traits for Q4.")
-                                    elif not selected_single_trait_q5:
-                                        st.warning("Please select a single trait for Q5.")
-                                    elif len(least_represented_traits_q6) != 3:
-                                        st.warning("Please select exactly 3 traits for Q6.")
-                                    elif len(selected_images_q7) != 3:
-                                        st.warning("Please select exactly 3 images for Q7.")
-                                    elif not selected_image_q8:
-                                        st.warning("Please select a single image for Q8.")
-                                    elif len(least_represented_images_q9) != 3:
-                                        st.warning("Please select exactly 3 images for Q9.")
-                                    elif len(selected_modes_q10) != 2:
-                                        st.warning("Please select exactly 2 modes for Q10.")
-                                    else:
-                                        top_two_colors, persona_name, score_counter = run_quiz()
-                                        st.write("Your top two colors are: ", ", ".join(top_two_colors))
-                                        st.write("Your persona name is: ", persona_name)
-                                        st.write("Total Scores for Each Color:")
-                                        for color in color_priority:
-                                            st.write(f"{color}: {score_counter[color]}")
+        if len(selected_traits_q1) != 3:
+            st.warning("Please select exactly 3 traits for Q1.")
+        elif not selected_single_trait_q2:
+            st.warning("Please select a single trait for Q2.")
+        elif len(least_represented_traits_q3) != 3:
+            st.warning("Please select exactly 3 traits for Q3.")
+        elif len(selected_traits_q4) != 3:
+            st.warning("Please select exactly 3 traits for Q4.")
+        elif not selected_single_trait_q5:
+            st.warning("Please select a single trait for Q5.")
+        elif len(least_represented_traits_q6) != 3:
+            st.warning("Please select exactly 3 traits for Q6.")
+        elif len(selected_images_q7) != 3:
+            st.warning("Please select exactly 3 images for Q7.")
+        elif not selected_image_q8:
+            st.warning("Please select a single image for Q8.")
+        elif len(least_represented_images_q9) != 3:
+            st.warning("Please select exactly 3 images for Q9.")
+        elif len(selected_modes_q10) != 2:
+            st.warning("Please select exactly 2 modes for Q10.")
+        else:
+            top_two_colors, persona_name, score_counter = run_quiz()
+            st.markdown("<h2>Quiz Results</h2>", unsafe_allow_html=True)
+            st.markdown(f"<h3>Your top two colors are: {', '.join(top_two_colors)}</h3>", unsafe_allow_html=True)
+            st.markdown(f"<h3>Your persona name is: {persona_name}</h3>", unsafe_allow_html=True)
+            st.markdown("<h3>Total Scores for Each Color:</h3>", unsafe_allow_html=True)
+            for color in color_priority:
+                st.markdown(f"<p>{color}: {score_counter[color]}</p>", unsafe_allow_html=True)
 
 # Set the random seed for each user session
 if 'random_seed' not in st.session_state:
