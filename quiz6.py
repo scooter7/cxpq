e1e9434162b817007ccb95a62eae15ab6d94e72d
scooter7 <scooter.vineburgh@gmail.com>
2023-07-14 22:56:49 -0500

    Update quiz6.py

--- quiz6.py
+++ quiz6.py
@@ -195,7 +195,7 @@ def personality_quiz():
 
     if len(selected_traits_q1) == 3:
         st.write("Q2. Of the 3 traits you selected, which single trait is most like you?")
-        selected_single_trait_q2 = st.selectbox("Q2. Of the 3 traits you selected, which single trait is most like you?", options=["Select a trait"] + selected_traits_q1, index=None, key="select_q2")
+        selected_single_trait_q2 = st.selectbox("Q2. Of the 3 traits you selected, which single trait is most like you?", options=["Select a trait"] + selected_traits_q1, index=0, key="select_q2")
 
         st.write("---")
 
@@ -247,7 +247,7 @@ def personality_quiz():
 
             if len(selected_traits_q4) == 3:
                 st.write("Q5. Of the 3 traits you selected, which single trait is most like you?")
-                selected_single_trait_q5 = st.selectbox("Q5. Of the 3 traits you selected, which single trait is most like you?", options=["Select a trait"] + selected_traits_q4, index=None, key="select_q5")
+                selected_single_trait_q5 = st.selectbox("Q5. Of the 3 traits you selected, which single trait is most like you?", options=["Select a trait"] + selected_traits_q4, index=0, key="select_q5")
 
 
                 st.write("---")
