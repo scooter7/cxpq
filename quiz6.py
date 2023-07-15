bbdecc113b5eb93a4e0dbf91429415551bec5ecf
scooter7 <scooter.vineburgh@gmail.com>
2023-07-14 22:52:36 -0500

    Update quiz6.py

--- quiz6.py
+++ quiz6.py
@@ -195,7 +195,7 @@ def personality_quiz():
 
     if len(selected_traits_q1) == 3:
         st.write("Q2. Of the 3 traits you selected, which single trait is most like you?")
-        selected_single_trait_q2 = st.radio("Q2. Of the 3 traits you selected, which single trait is most like you?", options=[""] + selected_traits_q1, index=None, key="radio_q2", format_func=lambda x: x if x else "Select a trait")
+        selected_single_trait_q2 = st.radio("Q2. Of the 3 traits you selected, which single trait is most like you?", options=["Select a trait"] + selected_traits_q1, index=None, key="radio_q2")
 
         st.write("---")
 
